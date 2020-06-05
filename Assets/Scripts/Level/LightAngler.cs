10920a8df4a0ba97aead865463f744291b80b922
epender <epender@umich.edu>
2020-06-05 16:58:31 -0400

    Clean up some stuff

--- Assets/Scripts/Level/LightAngler.cs
+++ Assets/Scripts/Level/LightAngler.cs
@@ -27,20 +27,14 @@ public class LightAngler : MonoBehaviour, SimpleLeverControlable {
         currentAngle = 0;
         myJoint = gameObject.AddComponent<RelativeJoint2D>();
         myJoint.connectedBody = controled.GetComponent<Rigidbody2D>();
-        myJoint.maxForce = 10000;
-        myJoint.maxTorque = 10000;
+        myJoint.maxForce = 10;
+        myJoint.maxTorque = 10;
         myJoint.autoConfigureOffset = false;
         myJoint.angularOffset = 0;
     }
 
-    public void Update() {
-        //currentAngle = controled.GetActualAngle() - angleLeft;
-    }
-
     public void MovePosition(int direction) {
         currentAngle = Mathf.Clamp(currentAngle + direction*speed, -angleLeft, angleRight);
-        // TODO: Don't do this, do something with joints
-        //controled.SetTargetAngle(initialAngle + currentAngle);
         myJoint.angularOffset = currentAngle;
     }
 
