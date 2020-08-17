64f5c4e467ccf050c60bcb59ee5c03d32068cee6
epender <epender@umich.edu>
2020-08-17 17:38:34 -0400

    Refactor Player.cs
    Add a StateMachine class to make state management more clear. Also split
    large functions into smaller ones with better names.

--- Assets/Scripts/Level/Player.cs
+++ Assets/Scripts/Level/Player.cs
@@ -1,120 +1,160 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour {
+namespace Player {
     public enum State {
         initial,
         inAir,
         onGround,
-        jumping
-    }
-    [SerializeField]
-    private State current;
-    Rigidbody2D rb;
-    [SerializeField]
-    private float airAccel;
-    [SerializeField]
-    private float airFriction;
-    [SerializeField]
-    private float groundAccel;
-    [SerializeField]
-    private float groundFriction;
-    [SerializeField]
-    private float maxSpeed;
-    [SerializeField]
-    private Collider2D groundDetector;
-    [SerializeField]
-    private Collider2D interactableDetector;
-    [SerializeField]
-    private Collider2D mainCollider;
-    [SerializeField]
-    private float jumpSpeed;
-    [SerializeField]
-    private float jumpTime;
-    [SerializeField]
-    private float forceTransferRatio;
-
-    private float timeOfLastJump;
-
-    void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        Refs.instance.cameraTracker.trackedObject = gameObject;
-        timeOfLastJump = -1000;
-        current = State.initial;
+        jumping,
+        interacting
     }
 
-    void FixedUpdate() {
-        DoStateMachine();
+    public class StateMachine {
 
-        int lr = (Input.GetKey(KeyCode.A) ? -1 : 0) + (Input.GetKey(KeyCode.D) ? 1 : 0);
-        var inputForce = (current == State.onGround ? groundAccel : airAccel) * Vector3.right * lr;
+        private State current = State.initial;
 
-        if (inputForce != Vector3.zero) {
-            inputForce = PhysicsHelper.TruncateForce(rb, maxSpeed, inputForce);
-            rb.AddForce(inputForce);
-            var standingOn = GetStandingOn();
-            foreach (var col in standingOn) {
-                col.attachedRigidbody.AddForce(-forceTransferRatio*inputForce/standingOn.Count);
+        public void TransitionState(Player player) {
+            if (current == State.onGround) {
+                if (player.JumpKeyPressed()) {
+                    current = State.jumping;
+                    player.OnFirstFrameOfJump();
+                    return;
+                }
+            } else if (current == State.jumping) {
+                if (player.CanStillJump()) {
+                    return;
+                }
+            }
+            current = player.IsOnGround() ? State.onGround : State.inAir;
+        }
+
+        public State Current() {
+            return current;
+        }
+    }
+
+    public class Player : MonoBehaviour {
+
+        private Rigidbody2D rb;
+        [SerializeField]
+        private StateMachine sm;
+        [SerializeField]
+        private float airAccel;
+        [SerializeField]
+        private float airFriction;
+        [SerializeField]
+        private float groundAccel;
+        [SerializeField]
+        private float groundFriction;
+        [SerializeField]
+        private float maxSpeed;
+        [SerializeField]
+        private Collider2D groundDetector;
+        [SerializeField]
+        private Collider2D interactableDetector;
+        [SerializeField]
+        private Collider2D mainCollider;
+        [SerializeField]
+        private float jumpSpeed;
+        [SerializeField]
+        private float jumpTime;
+        [SerializeField]
+        private float forceTransferRatio;
+
+        private float timeOfLastJump;
+
+        private void Start() {
+            rb = GetComponent<Rigidbody2D>();
+            Refs.instance.cameraTracker.trackedObject = gameObject;
+            timeOfLastJump = -1000;
+            sm = new StateMachine();
+        }
+
+        private void FixedUpdate() {
+            sm.TransitionState(this);
+
+            var playerForce = GetHorizontalInputForce();
+            rb.AddForce(playerForce);
+
+            ApplyForceToStandingOn(-playerForce*forceTransferRatio);
+
+            if (sm.Current() == State.jumping) {
+                Jump();
+            }
+
+            rb.AddForce(GetFrictionForce(playerForce));
+
+            int interaction = (Input.GetKey(KeyCode.Q) ? -1 : 0) + (Input.GetKey(KeyCode.E) ? 1 : 0);
+            if (interaction != 0) {
+                foreach (var lever in GetInteractable()) {
+                    lever.MovePosition(interaction);
+                }
             }
         }
 
-        if (current == State.jumping) {
+        private Vector2 GetHorizontalInputForce() {
+            int lr = (Input.GetKey(KeyCode.A) ? -1 : 0) + (Input.GetKey(KeyCode.D) ? 1 : 0);
+            Vector2 inputForce = (sm.Current() == State.onGround ? groundAccel : airAccel) * Vector3.right * lr;
+            return PhysicsHelper.TruncateForce(rb, maxSpeed, inputForce);
+        }
+
+        private void ApplyForceToStandingOn(Vector2 force) {
+            if (force!= Vector2.zero) {
+                var standingOn = GetStandingOn();
+                foreach (var col in standingOn) {
+                    col.attachedRigidbody.AddForce(force/standingOn.Count);
+                }
+            }
+        }
+
+        private List<Collider2D> GetStandingOn() {
+            var colliders = new List<Collider2D>();
+            groundDetector.GetContacts(colliders);
+
+            colliders.RemoveAll(col => col == mainCollider || col == interactableDetector || col.isTrigger);
+
+            return colliders;
+        }
+
+
+        public void Jump() {
             rb.AddForce(PhysicsHelper.GetNeededForce(rb, new Vector2(0,rb.velocity.y), Vector2.up*jumpSpeed));
         }
 
-        if (inputForce.x * rb.velocity.x <= 0) {
-            var frictionForce = -(current == State.onGround ? groundFriction : airFriction) * Vector3.right * rb.mass * rb.velocity.x;
-            rb.AddForce(frictionForce);
+        private Vector2 GetFrictionForce(Vector2 forceAppliedToPlayer) {
+            if (forceAppliedToPlayer.x * rb.velocity.x <= 0) {
+                return -(sm.Current() == State.onGround ? groundFriction : airFriction) * Vector3.right * rb.mass * rb.velocity.x;
+            }
+            return Vector2.zero;
+
         }
 
-        int interaction = (Input.GetKey(KeyCode.Q) ? -1 : 0) + (Input.GetKey(KeyCode.E) ? 1 : 0);
-        if (interaction != 0) {
-            foreach (var lever in GetInteractable()) {
-                lever.MovePosition(interaction);
-            }
-        }
-    }
-
-    IEnumerable<Lever> GetInteractable() {
-        var colliders = new List<Collider2D>();
-        interactableDetector.GetContacts(colliders);
-    
-        foreach (var col in colliders) {
-            if (col.TryGetComponent(out Lever lever)) {
-                yield return lever;
-            }
-        }
-    }
-
-    void DoStateMachine() {
-        if (current == State.onGround) {
-            if (Input.GetKey(KeyCode.Space)) {
-                current = State.jumping;
-                timeOfLastJump = Time.time;
-                return;
-            }
-        } else if (current == State.jumping) {
-            // If the player is still holding space, and it is within the
-            // specified time limit, stay jumping. This allows the player to
-            // alter the height of their jump
-            if (Time.time - timeOfLastJump < jumpTime && Input.GetKey(KeyCode.Space)) {
-                return;
+        private IEnumerable<Lever> GetInteractable() {
+            var colliders = new List<Collider2D>();
+            interactableDetector.GetContacts(colliders);
+        
+            foreach (var col in colliders) {
+                if (col.TryGetComponent(out Lever lever)) {
+                    yield return lever;
+                }
             }
         }
 
-        current = IsOnGround() ? State.onGround : State.inAir;
-    }
+        public bool IsOnGround() {
+            return GetStandingOn().Count > 0;
+        }
 
-    List<Collider2D> GetStandingOn() {
-        var colliders = new List<Collider2D>();
-        groundDetector.GetContacts(colliders);
+        public bool JumpKeyPressed() {
+            return Input.GetKey(KeyCode.Space);
+        }
 
-        colliders.RemoveAll(col => col == mainCollider || col == interactableDetector || col.isTrigger);
+        public void OnFirstFrameOfJump() {
+            timeOfLastJump = Time.time;
+        }
 
-        return colliders;
-    }
-
-    bool IsOnGround() {
-        return GetStandingOn().Count > 0;
+        public bool CanStillJump() {
+            return Time.time - timeOfLastJump < jumpTime && JumpKeyPressed();
+        }
     }
 }
