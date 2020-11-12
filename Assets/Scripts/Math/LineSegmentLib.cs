1688da88f68ee6d50d04faab5e72aed6742ab9d0
epender <epender@umich.edu>
2020-11-11 23:32:32 -0500

    Add some function to LineSegmentLib

--- Assets/Scripts/Math/LineSegmentLib.cs
+++ Assets/Scripts/Math/LineSegmentLib.cs
@@ -24,6 +24,61 @@ using UnityEngine;
 
 public static class LineSegmentLib
 {
+    public static Vector2 ClosestPointOnLineSeg(Vector2 l1, Vector2 l2, Vector2 point) {
+        float segLenSq = (l2 - l1).sqrMagnitude;  // i.e. |w-v|^2 -  avoid a sqrt
+        if (segLenSq == 0.0) {
+            return l1;
+        }
+        // Consider the line extending the segment, parameterized as v + t (w - v).
+        // We find projection of point p onto the line. 
+        // It falls where t = [(p-v) . (w-v)] / |w-v|^2
+        // We clamp t from [0,1] to handle points outside the segment vw.
+        float t = Mathf.Clamp01(Vector2.Dot(point - l1, l2 - l1) / segLenSq);
+        Vector2 projection = l1 + t * (l2 - l1);  // Projection falls on the segment
+        return projection;
+    }
+
+    // This appears to be working
+    public static bool LineSegmentsIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, out Vector2 intersection, float epsilon) {
+        if (LineSegmentsIntersection(p1, p2, p3, p4, out intersection)) {
+            return true;
+        }
+
+        intersection = Vector2.positiveInfinity;
+
+        bool Helper(Vector2 p, Vector2 pClose, ref Vector2 intersec) {
+            float pSqDist = (p - pClose).sqrMagnitude;
+            if ((p - pClose).sqrMagnitude < epsilon*epsilon) {
+                intersec = (p+pClose)/2;
+                return true;
+            }
+            return false;
+        }
+    
+        Vector2 p1Close = ClosestPointOnLineSeg(p3,p4, p1);
+        if (Helper(p1, p1Close, ref intersection)) {
+            return true;
+        }
+
+        Vector2 p2Close = ClosestPointOnLineSeg(p3,p4, p2);
+        if (Helper(p2, p2Close, ref intersection)) {
+            return true;
+        }
+
+        Vector2 p3Close = ClosestPointOnLineSeg(p1,p2, p3);
+        if (Helper(p3, p3Close, ref intersection)) {
+            return true;
+        }
+
+        Vector2 p4Close = ClosestPointOnLineSeg(p1,p2, p4);
+        if (Helper(p4, p4Close, ref intersection)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
     public static bool LineSegmentsIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, out Vector2 intersection)
     {
         intersection = Vector2.positiveInfinity;
@@ -42,6 +97,26 @@ public static class LineSegmentLib
         {
             return false;
         }
+
+        intersection.x = p1.x + u * (p2.x - p1.x);
+        intersection.y = p1.y + u * (p2.y - p1.y);
+
+        return true;
+    }
+
+    public static bool LineIntersection(Vector2 p1, Vector2 p2, Vector2 p3, Vector2 p4, out Vector2 intersection)
+    {
+        intersection = Vector2.positiveInfinity;
+
+        var d = (p2.x - p1.x) * (p4.y - p3.y) - (p2.y - p1.y) * (p4.x - p3.x);
+
+        if (d == 0.0f)
+        {
+            return false;
+        }
+
+        var u = ((p3.x - p1.x) * (p4.y - p3.y) - (p3.y - p1.y) * (p4.x - p3.x)) / d;
+        var v = ((p3.x - p1.x) * (p2.y - p1.y) - (p3.y - p1.y) * (p2.x - p1.x)) / d;
 
         intersection.x = p1.x + u * (p2.x - p1.x);
         intersection.y = p1.y + u * (p2.y - p1.y);
